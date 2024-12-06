2df4b7d618e26c7e153b93b5c74bdb71d4473121
lmangani <lorenzo.mangani@gmail.com>
2024-12-06 16:57:17 +0000

    tsid

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 extern crate duckdb;
 extern crate duckdb_loadable_macros;
 extern crate libduckdb_sys;
+use tsid::create_tsid;
 
 use duckdb::{
     core::{DataChunkHandle, Inserter, LogicalTypeHandle, LogicalTypeId},
@@ -14,45 +15,30 @@ use std::{
     ffi::{c_char, CString},
 };
 
+// Scalar Function
 #[repr(C)]
-struct HelloBindData {
-    name: *mut c_char,
-}
-
-impl Free for HelloBindData {
-    fn free(&mut self) {
-        unsafe {
-            if self.name.is_null() {
-                return;
-            }
-            drop(CString::from_raw(self.name));
-        }
-    }
-}
+struct TsidBindData;
 
 #[repr(C)]
-struct HelloInitData {
+struct TsidInitData {
     done: bool,
 }
 
-struct HelloVTab;
+struct TsidVTab;
 
-impl Free for HelloInitData {}
+impl Free for TsidBindData {}
+impl Free for TsidInitData {}
 
-impl VTab for HelloVTab {
-    type InitData = HelloInitData;
-    type BindData = HelloBindData;
+impl VTab for TsidVTab {
+    type InitData = TsidInitData;
+    type BindData = TsidBindData;
 
-    unsafe fn bind(bind: &BindInfo, data: *mut HelloBindData) -> Result<(), Box<dyn std::error::Error>> {
-        bind.add_result_column("value", LogicalTypeHandle::from(LogicalTypeId::Varchar));
-        let param = bind.get_parameter(0).to_string();
-        unsafe {
-            (*data).name = CString::new(param).unwrap().into_raw();
-        }
+    unsafe fn bind(bind: &BindInfo, _: *mut TsidBindData) -> Result<(), Box<dyn std::error::Error>> {
+        bind.add_result_column("tsid", LogicalTypeHandle::from(LogicalTypeId::Varchar));
         Ok(())
     }
 
-    unsafe fn init(_: &InitInfo, data: *mut HelloInitData) -> Result<(), Box<dyn std::error::Error>> {
+    unsafe fn init(_: &InitInfo, data: *mut TsidInitData) -> Result<(), Box<dyn std::error::Error>> {
         unsafe {
             (*data).done = false;
         }
@@ -60,20 +46,15 @@ impl VTab for HelloVTab {
     }
 
     unsafe fn func(func: &FunctionInfo, output: &mut DataChunkHandle) -> Result<(), Box<dyn std::error::Error>> {
-        let init_info = func.get_init_data::<HelloInitData>();
-        let bind_info = func.get_bind_data::<HelloBindData>();
-
+        let init_info = func.get_init_data::<TsidInitData>();
         unsafe {
             if (*init_info).done {
                 output.set_len(0);
             } else {
                 (*init_info).done = true;
                 let vector = output.flat_vector(0);
-                let name = CString::from_raw((*bind_info).name);
-                let result = CString::new(format!("Rusty Quack {} 🐥", name.to_str()?))?;
-                // Can't consume the CString
-                (*bind_info).name = CString::into_raw(name);
-                vector.insert(0, result);
+                let tsid = create_tsid();
+                vector.insert(0, CString::new(tsid.to_string())?);
                 output.set_len(1);
             }
         }
@@ -81,15 +62,15 @@ impl VTab for HelloVTab {
     }
 
     fn parameters() -> Option<Vec<LogicalTypeHandle>> {
-        Some(vec![LogicalTypeHandle::from(LogicalTypeId::Varchar)])
+        None
     }
 }
 
 const EXTENSION_NAME: &str = env!("CARGO_PKG_NAME");
 
-#[duckdb_entrypoint_c_api(ext_name = "rusty_quack", min_duckdb_version = "v0.0.1")]
+#[duckdb_entrypoint_c_api(ext_name = "tsid", min_duckdb_version = "v0.0.1")]
 pub unsafe fn extension_entrypoint(con: Connection) -> Result<(), Box<dyn Error>> {
-    con.register_table_function::<HelloVTab>(EXTENSION_NAME)
-        .expect("Failed to register hello table function");
+    con.register_table_function::<TsidVTab>("tsid")
+        .expect("Failed to register tsid table function");
     Ok(())
 }
